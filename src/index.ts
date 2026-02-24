b851dd321075c3c68ca44ae8a2da93db83f6c004
studschmda1533 <daniel.schmerber@stud.th-rosenheim.de>
2026-02-24 07:46:30 +0100

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { readSchematic } from "./schematic/reader/spongeV3Schematicreader";
 export { SpongeV3SchematicWriter } from "./schematic/writer/spongeV3SchematicWriter";
+import {Schem} from "./schematic/Schematic";
+import {Block} from "./schematic/Schematic";
+export {Schem,Block};
+import {SchematicWrapper} from "./schematic/Schematic";
+import {BlockWrapper} from "./schematic/Schematic";
+export {SchematicWrapper,BlockWrapper};
