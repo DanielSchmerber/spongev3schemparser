888a7dd7e065bd21dbd277cacb05c7f8eff5f6fe
studschmda1533 <daniel.schmerber@stud.th-rosenheim.de>
2026-02-25 04:11:29 +0100

    writing is also forced sync

--- dist/index.js
+++ dist/index.js
@@ -1,52 +1,11 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BlockWrapper = exports.SchematicWrapper = exports.SpongeV3SchematicWriter = exports.readSchematic = void 0;
-const spongeV3Schematicreader_1 = require("./schematic/reader/spongeV3Schematicreader");
-var spongeV3Schematicreader_2 = require("./schematic/reader/spongeV3Schematicreader");
-Object.defineProperty(exports, "readSchematic", { enumerable: true, get: function () { return spongeV3Schematicreader_2.readSchematic; } });
+var spongeV3Schematicreader_1 = require("./schematic/reader/spongeV3Schematicreader");
+Object.defineProperty(exports, "readSchematic", { enumerable: true, get: function () { return spongeV3Schematicreader_1.readSchematic; } });
 var spongeV3SchematicWriter_1 = require("./schematic/writer/spongeV3SchematicWriter");
 Object.defineProperty(exports, "SpongeV3SchematicWriter", { enumerable: true, get: function () { return spongeV3SchematicWriter_1.SpongeV3SchematicWriter; } });
 const Schematic_1 = require("./schematic/Schematic");
 Object.defineProperty(exports, "SchematicWrapper", { enumerable: true, get: function () { return Schematic_1.SchematicWrapper; } });
 const Schematic_2 = require("./schematic/Schematic");
 Object.defineProperty(exports, "BlockWrapper", { enumerable: true, get: function () { return Schematic_2.BlockWrapper; } });
-const fs = __importStar(require("node:fs"));
-async function test() {
-    let schem = await (0, spongeV3Schematicreader_1.readSchematic)(fs.readFileSync("../Cringelig.schem"));
-    let testBlock = [...schem.getBlocks()].find((x) => x.block.getData());
-    console.log(JSON.stringify(testBlock === null || testBlock === void 0 ? void 0 : testBlock.block.getSimpleData()));
-}
-test();
--- src/schematic/writer/spongeV3SchematicWriter.ts
+++ src/schematic/writer/spongeV3SchematicWriter.ts
@@ -4,7 +4,7 @@ import * as fs from "node:fs";
 import { promisify } from 'node:util';
 // Node built-in gzip
 import { gzip as nodeGzip } from "node:zlib";
-
+import { gzipSync } from "node:zlib";
 // Zopfli gzip
 import { gzip as zopfliGzip } from "node-zopfli";
 export type CompressionOptions =
@@ -24,96 +24,66 @@ export class SpongeV3SchematicWriter {
         this.compression = compression
     }
 
-    async writeSchematic(){
-        let pallet = this.createPallet();
-        let palletObj = [...pallet.entries()].reduce(
+    writeSchematic(): Buffer {
+        const pallet = this.createPallet();
+
+        const palletObj = [...pallet.entries()].reduce(
             (acc, [key, value]) => {
                 // @ts-ignore
                 acc[key] = nbt.int(value)
                 return acc
             },
-            Object.create(null) // ← safe accumulator
+            Object.create(null)
         )
 
-        let blockEntities = []
-        let intArray = []
-        //Write blockdata
-        for(let block of iterateBlocks(this.schem)){
-            let index = block.x + (block.z + block.y * this.schem.getLength()) * this.schem.getWidth()
-            let blockPallet = pallet.get(block.block.getMaterial())!
+        const blockEntities: any[] = []
+        let intArray: number[] = []
+
+        for (let block of iterateBlocks(this.schem)) {
+            const index =
+                block.x +
+                (block.z + block.y * this.schem.getLength()) *
+                this.schem.getWidth()
+
+            const blockPallet = pallet.get(block.block.getMaterial())!
 
             if (block.block.getData()) {
-                const data = block.block.getData()!
-
-                blockEntities.push(
-
-                    {
-                        Pos: nbt.intArray([block.x, block.y, block.z]),
-                        //@ts-ignore
-                        Id: block.block.getData()!["id"],
-                        Data: nbt.comp(block.block.getData()!)
-                    }
-
-
-                )
-            }
-
-            intArray[index] = (blockPallet)
-        }
-        intArray = intArrayToVarintByteArray(intArray)
-        let schemnbt = nbt.comp(
-            {
-                Schematic:nbt.comp({
-                    Height: nbt.short(this.schem.getHeight()),
-                    Width: nbt.short(this.schem.getWidth()),
-                    Length: nbt.short(this.schem.getLength()),
-                    Version: nbt.int(3),
-                    Offset : nbt.intArray(this.schem.getOffset()),
-                    DataVersion : nbt.int(this.schem.getDataVersion()),
-                    Blocks: nbt.comp(
-                        {
-                            Palette: nbt.comp(palletObj),
-                            BlockEntities:nbt.list(nbt.comp(blockEntities)),
-                            Data:nbt.byteArray(intArray),
-                        }
-                    ),
-                    Entities:nbt.list(nbt.comp([])),
+                // @ts-ignore
+                blockEntities.push({
+                    Pos: nbt.intArray([block.x, block.y, block.z]),
+                    // @ts-ignore
+                    Id: block.block.getData()!["id"],
+                    Data: nbt.comp(block.block.getData()!)
                 })
             }
-        )
 
+            intArray[index] = blockPallet
+        }
 
+        intArray = intArrayToVarintByteArray(intArray)
 
-
-
+        const schemnbt = nbt.comp({
+            Schematic: nbt.comp({
+                Height: nbt.short(this.schem.getHeight()),
+                Width: nbt.short(this.schem.getWidth()),
+                Length: nbt.short(this.schem.getLength()),
+                Version: nbt.int(3),
+                Offset: nbt.intArray(this.schem.getOffset()),
+                DataVersion: nbt.int(this.schem.getDataVersion()),
+                Blocks: nbt.comp({
+                    Palette: nbt.comp(palletObj),
+                    BlockEntities: nbt.list(nbt.comp(blockEntities)),
+                    Data: nbt.byteArray(intArray),
+                }),
+                Entities: nbt.list(nbt.comp([])),
+            }),
+        })
 
         // @ts-ignore
         const raw = nbt.writeUncompressed(schemnbt)
-        let buf: Buffer
 
-        if (this.compression.type === "zopfli") {
-
-
-            buf = await zopfliGzip(raw, {
-                verbose: false,
-                numiterations: this.compression.numiterations ?? 2,
-            }) as Buffer
-
-        } else {
-
-            const gzipAsync = promisify(nodeGzip)
-
-            buf = await gzipAsync(raw)
-        }
-
-
-
-
-        //@ts-ignore
-        fs.writeFileSync("schem.schem", buf)
-
-
-
+        // 🔥 FULLY SYNC COMPRESSION
+        return gzipSync(raw)
     }
 
     createPallet(){
